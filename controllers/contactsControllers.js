67168ba7005769d39f6db87225500265c341180b
OlhaBuhun <115622612+OlhaBuhun@users.noreply.github.com>
2024-08-19 14:53:41 +0300

    pagination

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -5,8 +5,10 @@ import HttpError from "../helpers/HttpError.js";
 import { ctrlWrapper } from "../decorators/index.js";
 
 const getAllContacts = async (req, res) => {
-	const {_id: owner} = req.user;
-	const result = await Contact.find({owner}, "-createdAt -updatedAt");
+	const { _id: owner } = req.user;
+	const { page = 1, limit = 10 } = req.query;
+	const skip = (page - 1) * limit;
+	const result = await Contact.find({ owner }, "-createdAt -updatedAt", { skip, limit }).populate("owner", "email");
 	res.json(result);
 };
 
